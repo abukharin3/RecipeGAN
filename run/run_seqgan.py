428fc46c3b933d4c962aa2b03c27d5b83e5e9ea8
Alexander Bukharin <abukharin@lawn-128-61-23-137.lawn.gatech.edu>
2022-05-02 09:33:54 -0400

    Time adv epoch

--- run/run_seqgan.py
+++ run/run_seqgan.py
@@ -38,7 +38,7 @@ CUDA = int(True)
 oracle_pretrain = int(True)
 gen_pretrain = int(False)
 dis_pretrain = int(False)
-MLE_train_epoch = 80
+MLE_train_epoch = 0
 ADV_train_epoch = 100
 tips = 'SeqGAN experiments'
 
